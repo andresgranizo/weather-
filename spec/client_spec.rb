5ec347910dbac94290fdbbcd7a6f2743a238654b
Andres Granizo <cesarandres.555.666@gmail.com>
2019-08-07 17:11:37 +0000

    Update Spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,9 +6,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<APP_ID>') { ENV['APP_ID'] }
-  config.default_cassette_options = {
-    :match_requests_on => [:method,
-      VCR.request_matchers.uri_without_param(:timestamp)]}
 end
 
 describe Client do
